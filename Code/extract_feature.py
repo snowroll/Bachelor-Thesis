ea5d9a63a4db2ef6a4c19b3bb0c3660b559fcf9e
chaihj15 <chaihj15@chaihj15deMacBook-Pro.local>
2019-02-20 11:05:42 +0800

    extract features done

--- Code/extract_feature.py
+++ Code/extract_feature.py
@@ -2,12 +2,19 @@
 import re
 filename = "benign.txt"
 
+js_keywords = []
+keyword_dict = {}
+with open("features.txt", 'r') as f:  # js语言的关键字作为特征
+    for i in f:
+        js_keywords.append(i.strip("\n"))
+
 comment = r'[^:]//(.*)'  # 短注释
 comment_long = r'/\*(.*?)\*/'  # 长注释
 repet = r'([0-9a-zA-Z])\1{1,}'
 comment_words = 0
 call = r'[a-zA-Z0-9_]+\([a-zA-Z0-9_, \-\'\"]*\)'  # 方法调用
 argument = r'[a-zA-Z0-9_]+\((.*?)\)'  # 方法参数
+_hex = r'[0\\]x[0-9a-fA-F]+'  # 16进制数
 operators = ["+", "-", "*", "/", "=", "+=", "*=", "%=", "/=", "-=", "++", "--"]
 len_characters = 0
 script_str = ""
@@ -44,8 +51,10 @@ with open(filename, 'r') as f:
         len_characters += len(i)
     per_line = len_characters / lines  # 每行的字符数
     tokens = script_str.split()
+
     string_num = len(tokens)  # 单词个数
     avg_string = len_characters / string_num  # 每个单词平均长度
+
     white_space = script_str.count(" ") / len_characters  # 空白字符占比
     com2 = re.findall(comment_long, script_str, re.S | re.M)  # 多行注释
     for i in com2:
@@ -53,9 +62,11 @@ with open(filename, 'r') as f:
         comment_words += len(i.split())  # comment的数量
     avg_comment = comment_num / lines  # 平均每行的comment数量
     per_comment = 1 - comment_words / string_num  # 不在comment中的单词占比
+
     operator_num = 0
     for i in operators:
         operator_num += script_str.count(i)
+
     methods = re.findall(call, script_str, re.S | re.M)  # 方法调用
     methods_num = len(methods)
     argument_len = 0
@@ -64,15 +75,25 @@ with open(filename, 'r') as f:
         for j in arguments:
             if len(j):
                 argument_len += len(j)
-                # print(j)
+
     per_argument = argument_len / methods_num  # 每个方法的平均参数长度
     read_num = 0  # 可读单词比例
     for i in tokens:
         if isreadable(i):
             read_num += 1
-    print(read_num / string_num)
-    print(methods_num, per_argument)
-    print("operator_num", operator_num)
-    print(comment_num, avg_comment, per_comment)
-    print(len_characters, lines, per_line, avg_string, white_space)
+    ratio_read = read_num / string_num
+
+    hexadecimal_num = len(re.findall(_hex, script_str, re.S | re.M))  # 16进制的个数
+
+    for i in js_keywords:
+        keyword_dict[i] = script_str.count(i)
+    
+    features_dict = {"len_characters": len_characters, "per_line": per_line, "lines": lines, "string_num": string_num, \
+        "hexadecimal_num": hexadecimal_num, "ratio_read": ratio_read, "white_space": white_space, "methods_num": methods_num, \
+        "avg_string": avg_string, "avg_comment": avg_comment, "comment_num": comment_num, "per_comment": per_comment, \
+        "operator_num": operator_num, "per_argument": per_argument}
+
+    features_dict.update(keyword_dict)  # 合并关键词特征和计算后的特征
+
+    print(features_dict)
 
