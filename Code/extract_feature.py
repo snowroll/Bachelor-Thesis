353fd5a6440dff24e56b556af14622850765d15b
chaihj15 <chaihj15@chaihj15deMacBook-Pro.local>
2019-03-27 11:41:17 +0800

    Detect Part Done

--- Code/extract_feature.py
+++ Code/extract_feature.py
@@ -3,14 +3,12 @@ import re
 import os  # 读取文件
 import pandas as pd  # csv文件
 
-
-filename = "benign.txt"
-
 js_keywords = []
-with open("features.txt", 'r') as f:  # js语言的关键字作为特征
+with open("../Feature/features_before.txt", 'r') as f:  # js语言的关键字作为特征
     for i in f:
         js_keywords.append(i.strip("\n"))
 
+double_number = r'[0-9]{2,5}'  # 数字
 comment = r'[^:]//(.*)'  # 短注释
 comment_long = r'/\*(.*?)\*/'  # 长注释
 repet = r'([0-9a-zA-Z])\1{1,}'
@@ -21,7 +19,7 @@ operators = ["+", "-", "*", "/", "=", "+=", "*=", "%=", "/=", "-=", "++", "--"]
 
 
 def isreadable(word):  # 判断一个单词是否可读
-    if len(word) > 30:
+    if len(word) > 25:
         return False
     alpha_num = 0
     vowel_num = 0
@@ -41,94 +39,143 @@ def isreadable(word):  # 判断一个单词是否可读
     return True
 
 def get_features(filename, _property):  # 手工添加最后一个特征， 良性或恶意
-    comment_words = 0
     len_characters = 0
     script_str = ""
+    long_unread = 0
+    number_count = 0
+    # ratio_number = 0
     keyword_dict = {}
+    last_name = filename.split("/")[-1]
 
     with open(filename, 'r') as f:
         lines = 0
+        tab_lines = 0
         comment_num = 0
         for i in f:
             com = re.findall(comment, i, re.S | re.M) # 单行注释
+            double_number_n = re.findall(double_number, i, re.S | re.M)  #数字
+            number_count += len(double_number_n)
             if len(com):
-                comment_num += 1
-                for i in com:
-                    comment_words += len(i.split())
+                comment_num += len(com)
+            if i[0:2] == '  ':
+                tab_lines += 1
             lines += 1
             script_str += i
             len_characters += len(i)
+            if(len(i) >= 1000):
+                token = re.split("[,./?';\"\{\}\[\])( =\%-]", i)
+                for j in token:
+                    if(len(j) >= 1000):
+                        long_unread += 1
         per_line = 0 if not lines else len_characters / lines  # 每行的字符数
-        tokens = script_str.split()
+        tab_ratio = tab_lines / lines  # 有tab的代码行占比
+        tokens = re.split("[,./?';\"\{\}\[\])( +=\%-]", script_str)  # 将每行分解为单个词语的集合
 
-        string_num = len(tokens)  # 单词个数
-        avg_string = 0 if not string_num else len_characters / string_num  # 每个单词平均长度
+        signle_chr = 0  # 单个字母字符  a b c...
+        alpha_dig_num = 0  # w32 类似符号的集合
+        for i in tokens:
+            if len(i) == 1 and str.isalpha(i):
+                signle_chr += 1
+            if len(i) < 4 and len([j for j in i if j.isdigit()]):
+                alpha_dig_num += 1         
+
+        word_num = len(tokens)  # 单词个数
+        # ratio_number = number_count / word_num
+        
+        avg_string = 0 if not word_num else len_characters / word_num  # 每个单词平均长度
 
         white_space = 0 if not len_characters else script_str.count(" ") / len_characters  # 空白字符占比
-        com2 = re.findall(comment_long, script_str, re.S | re.M)  # 多行注释
-        for i in com2:
-            comment_num += i.count("\n")  # comment
-            comment_words += len(i.split())  # comment的数量
+
+        com_mul = re.findall(comment_long, script_str, re.S | re.M)  # 多行注释
+        for i in com_mul:
+            comment_num += len(i)
         avg_comment = 0 if not lines else comment_num / lines  # 平均每行的comment数量
-        per_comment = 0 if not string_num else 1 - comment_words / string_num  # 不在comment中的单词占比
 
         operator_num = 0
         for i in operators:
             operator_num += script_str.count(i)
+        ratio_operator = (0, operator_num / word_num)[bool(word_num)]  # operator_num
 
         methods = re.findall(call, script_str, re.S | re.M)  # 方法调用
         methods_num = len(methods)
+
         argument_len = 0
         for i in methods:
             arguments = re.findall(argument, i, re.S | re.M)  # 方法参数
             for j in arguments:
                 if len(j):
                     argument_len += len(j)
+        per_argument =  0 if not methods_num else argument_len / methods_num  # 每个方法的平均参数长度
 
-        per_argument = 0 if not methods_num else argument_len / methods_num  # 每个方法的平均参数长度
         read_num = 0  # 可读单词比例
         for i in tokens:
             if isreadable(i):
                 read_num += 1
-        ratio_read = 0 if not string_num else read_num / string_num
+        ratio_read = 0 if not word_num else read_num / word_num
 
         hexadecimal_num = len(re.findall(_hex, script_str, re.S | re.M))  # 16进制的个数
 
         for i in js_keywords:
             keyword_dict[i] = script_str.count(i)
         
-        features_dict = {"property": _property, "len_characters": len_characters, "per_line": per_line, "lines": lines, "string_num": string_num, \
-            "hexadecimal_num": hexadecimal_num, "ratio_read": ratio_read, "white_space": white_space, "methods_num": methods_num, \
-            "avg_string": avg_string, "avg_comment": avg_comment, "comment_num": comment_num, "per_comment": per_comment, \
-            "operator_num": operator_num, "per_argument": per_argument}
+        features_dict = \
+        {"filename":last_name,"property": _property, "len_characters": len_characters, "per_line": per_line, "lines": lines, "tab_ratio" : tab_ratio, \
+         "word_num": word_num, "ratio_space": white_space, "signle_chr": signle_chr, "alpha_dig_num": alpha_dig_num, "methods_num": methods_num, \
+         "hex_num": hexadecimal_num, "comment_num": comment_num, "avg_comment": avg_comment,  "avg_argument": per_argument, \
+         "ratio_read": ratio_read,  "ratio_operator": ratio_operator}
 
         features_dict.update(keyword_dict)  # 合并关键词特征和计算后的特征
+        # for i in features_dict:
+        #     print(i)
         return features_dict
 
-def extract_feature(index_name):  # 训练集中提取样本features
-    path = "/Users/chaihj15/Desktop/Data/" + index_name # 文件夹目录
-    files= os.listdir(path)  # 得到文件夹下的所有文件名称
+def extract_feature():  # 训练集中提取样本features
     i = 0
+    # file_list = []
+    # for i in range(1, 11):
+    #     file_list.append(str(i))
+    # file_list.append("blackJS")
+
+    df = pd.DataFrame(columns=('filename', 'property', 'len_characters', 'per_line', 'lines', 'tab_ratio', 'word_num', 'ratio_space', 'signle_chr', 'alpha_dig_num', 'methods_num', 'hex_num', \
+        'comment_num', 'avg_comment', 'avg_argument', 'ratio_read', 'ratio_operator', 'var','return','function','term','length','else','new','null','prototype','type','typeof','Module','style', \
+        'fn','com','document','class','url','text','title','src','ie','data','href','cn','window','if','name','location','www','link','contentStr','titleStr','WEA','images','true',\
+        'false','js','Math','event','element','break','parentNode','jQuery','position','html','default'))
+    # for index_name in file_list:
+    #     print(index_name)
+    path = "/Users/chaihj15/Desktop/Data/test_data" #+ index_name  # 文件夹目录
+    files= os.listdir(path)  # 得到文件夹下的所有文件名称
     for f in files:  # 遍历文件夹
         file_path = os.path.join(path, f)
         if not os.path.isdir(file_path):  # 判断是否是文件夹，不是文件夹才打开
             # print(file_path)
             try:
-                _dict = get_features(file_path, "benign")
+                if "_black" in file_path:
+                    _dict = get_features(file_path, "1")
+                else:
+                    _dict = get_features(file_path, "0")
             except UnicodeDecodeError as e:
                 continue
-            if i == 0:
-                new_dict = {}
-                for key in _dict:
-                    new_dict[key] = [_dict[key]]
-                    # print(_dict[key], new_dict[key])
-                df = pd.DataFrame(new_dict)
-                df = df[list(_dict.keys())]
-            else:
-                df.loc[i] = list(_dict.values())  
+            df.loc[i] = list(_dict.values())  
+            if i % 100 == 0:
+                print(i)
             i += 1
-    df.to_csv(path + "/" + "data.csv", index = True, sep=',')
+        else:
+            child_files = os.listdir(file_path)  #是文件夹则读取文件夹下的内容
+            for f in child_files:  # 遍历文件夹
+                child_file_path = os.path.join(file_path, f)
+                if not os.path.isdir(child_file_path):  # 判断是否是文件夹，不是文件夹才打开
+                    # print(file_path)
+                    try:
+                        if "_black" in child_file_path:
+                            _dict = get_features(child_file_path, "1")
+                        else:
+                            _dict = get_features(child_file_path, "0")
+                    except UnicodeDecodeError as e:
+                        continue
+                    df.loc[i] = list(_dict.values())  
+                    if i % 100 == 0:
+                        print(i)
+                    i += 1
+    df.to_csv("/Users/chaihj15/Desktop/Data/test_data/" + "data.csv", index = False, sep=',')
 
-for i in range(2,11): 
-    print(i)
-    extract_feature(str(i))
+extract_feature()
--- Detect/random-forest.py
+++ Detect/random-forest.py
@@ -5,59 +5,69 @@ import matplotlib.pyplot as plt
 from sklearn import svm, datasets, ensemble
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
+from sklearn.externals import joblib
 
-from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import roc_curve, auc, roc_auc_score
 from sklearn.model_selection import train_test_split, StratifiedKFold
+from sklearn.grid_search import GridSearchCV
 import pandas as pd
 
 def read_data(filename):  # 读取训练数据
     Feature_Mat = []
     Label_Mat = []
+    File_Mat = []
     data = pd.read_csv(filename)
     data_count = len(data)
     for i in range(data_count):
         row = pd.Series.tolist(data.iloc[i])
-        Feature_Mat.append(row[1:])
-        Label_Mat.append(row[0])
+        Feature_Mat.append(row[2:])  # 暂时去掉关键词
+        Label_Mat.append(row[1])
+        File_Mat.append(row[0])
     Feature_Mat = np.array(Feature_Mat)
     Label_Mat = np.array(Label_Mat)
-    return Feature_Mat, Label_Mat
+    File_Mat = np.array(File_Mat)
+    return Feature_Mat, Label_Mat, File_Mat
 
-X, Y = read_data('./data.csv')
+train_X, train_Y, train_F = read_data('../Data/train_data/data.csv')
+test_X, test_Y, test_F = read_data('../Data/test_data/data.csv')
 scaler = StandardScaler()  # 标准化转换
-scaler.fit(X)  # 训练标准化对象
-X = scaler.transform(X)  # 转换数据集
-print(X[1])
-random_state = np.random.RandomState(0)
-# shuffle and split training and test sets
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=.3,random_state=0)
+scaler.fit(train_X)  # 训练标准化对象
+train_X = scaler.transform(train_X)  # 转换数据集
+scaler_test = StandardScaler()
+scaler_test.fit(test_X)
+test_X = scaler_test.transform(test_X)
+
 print("data is ready")
 
-clf = RandomForestClassifier()
-clf.fit(X_train, Y_train)
-Y_score = clf.predict(X_test)
-# random_tree = ensemble.RandomForestClassifier(n_estimators=100, criterion='gini', max_depth=20,min_samples_split=20, \
-#     min_samples_leaf=20, min_weight_fraction_leaf=0.0,max_features='auto', max_leaf_nodes=3, min_impurity_split=1e-07, \
-#     bootstrap=True, oob_score=False, n_jobs=1, random_state=None, verbose=0,warm_start=False, class_weight=None)
+# 开始训练 调参
+clf = RandomForestClassifier(n_estimators=50, min_samples_split=20 )
+clf.fit(train_X, train_Y)
+Y_score = clf.predict(test_X)
+print("max_depth ", clf.max_depth, " min_sample_split ", clf.min_samples_split, " min_sample_leaf ", clf.min_samples_split, \
+    "max_fetures ", clf.max_features)
+print(clf.feature_importances_) 
+joblib.dump(clf, 'train_model.m')
 
-# svm = svm.SVC(kernel='linear', probability=True, random_state=random_state)
-# print("svm is ok")
-# Y_score = random_tree.fit
-# Y_score = svm.fit(X_train, Y_train).decision_function(X_test)
+
+
+#n_estimators 50  'max_depth': 9,  'min_samples_leaf': 10, 'min_samples_split': 80  max_features': 7
+#n_estimators 50 min_samples_split 20   96%
+
+# print("AUC socre", roc_auc_score(train_Y, Y_score))
+
 print("train is ok")
-with open('ans-t-p.txt', 'w') as f:
+with open('test/ans-t-p.txt', 'w') as f:
     j = 0
-    for i in range(len(Y_test)):
-        f.write(str(Y_test[i]) + " " + str(Y_score[i]) + '\n')
-        if Y_test[i] == Y_score[i]:
+    for i in range(len(test_Y)):
+        f.write(str(test_F[i]) + " " + str(test_Y[i]) + " " + str(Y_score[i]) + '\n')
+        if test_Y[i] == Y_score[i]:
             j += 1
-    f.write(str(j / len(Y_test)))
+    f.write(str(j / len(test_Y)))
 
 # Compute ROC curve and ROC area for each class
-fpr,tpr,threshold = roc_curve(Y_test, Y_score) ###计算真正率和假正率
+fpr,tpr,threshold = roc_curve(test_Y, Y_score) ###计算真正率和假正率
 print('fpr ', fpr, 'tpr ', tpr, 'threshold', threshold)
 roc_auc = auc(fpr,tpr) ###计算auc的值
-print("compute roc")
 
 plt.figure()
 lw = 2
@@ -71,7 +81,7 @@ plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
 plt.title('Receiver operating characteristic example')
 plt.legend(loc="lower right")
-plt.savefig("answer1.png")
+plt.savefig("test/answer1.png")
 plt.show()
 
 
